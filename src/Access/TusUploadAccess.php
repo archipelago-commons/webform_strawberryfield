65c4a0ae5bd6283a2d9f3d7b734f86aefcf36efb
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2025-02-04 09:20:06 -0500

    WIP TUS

---
+++ src/Access/TusUploadAccess.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Drupal\webform_strawberryfield\Access;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\webform\WebformInterface;
+use Drupal\webform\WebformSubmissionInterface;
+use Drupal\webform_strawberryfield\Plugin\WebformHandler\strawberryFieldharvester;
+
+/**
+ * Defines the custom access control handler for the webform submission entities.
+ */
+class TusUploadAccess {
+
+  /**
+   * Check that webform submission has a strawberryfield webform handler and the user can create bundled Nodes
+   * @param \Drupal\webform\WebformSubmissionInterface $webform_submission
+   *   A webform submission.
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   Run access checks for this account.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The access result.
+   */
+  public static function checkAccess(WebformInterface $webform, string $key, AccountInterface $account) {
+    $possible_upload_element = $webform->getElement($key);
+    $all_upload_elements = $webform->getElementsManagedFiles();
+    $access_result = AccessResult::allowed();
+    //allowedIf()
+    // $access_result->addCacheTags(['config:webform.settings']);
+    return $access_result;
+  }
+
+}
---
+++ src/WebformStrawberryTusServerService.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace Drupal\webform_strawberryfield;
+
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\webform_strawberryfield\Event\WebformStrawberryFieldTusUploadedEvent;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
+use TusPhp\Cache\AbstractCache;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\File\FileSystemInterface;
+use Drupal\Core\Utility\Token;
+use Drupal\file\FileUsage\FileUsageInterface;
+use TusPhp\Events\TusEvent;
+use TusPhp\Events\UploadComplete;
+use TusPhp\Tus\Server;
+use Drupal\file\Entity\File;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+/**
+ * Class TusServer.
+ */
+class WebformStrawberryTusServerService {
+
+
+  /**
+   * Instance of Token.
+   *
+   * @var \Drupal\Core\Utility\Token
+   */
+  protected $token;
+
+  /**
+   * Instance of FileUsage.
+   *
+   * @var \Drupal\file\FileUsage\FileUsageInterface
+   */
+  protected $fileUsage;
+
+  /**
+   * Instance of EntityTypeManager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Instance of FileSystem.
+   *
+   * @var \Drupal\Core\File\FileSystemInterface
+   */
+  protected $fileSystem;
+
+  /**
+   * Tus settings config.
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $config;
+
+  /**
+   * Tus cache directory URI.
+   *
+   * @var string
+   */
+  protected $tusCacheDir;
+
+  /**
+   * Instance of AccountProxy.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Instance of EventDispatcher.
+   *
+   * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
+   */
+  protected $eventDispatcher;
+
+  /**
+   * @var \TusPhp\Cache\AbstractCache
+   */
+  private AbstractCache $tusCacheService;
+
+  /**
+   * Constructs a new TusServer object.
+   */
+  public function __construct(Token $token, FileUsageInterface $file_usage, EntityTypeManagerInterface $entity_type_manager, FileSystemInterface $file_system, ConfigFactoryInterface $config_factory, AccountProxyInterface $current_user, EventDispatcherInterface $event_dispatcher, AbstractCache $WebformStrawberryTusCacheService) {
+    $this->token = $token;
+    $this->fileUsage = $file_usage;
+    $this->entityTypeManager = $entity_type_manager;
+    $this->fileSystem = $file_system;
+    $this->config = $config_factory->get('webform_strawberryfield');
+    $this->tusCacheDir = $this->config->get('tus_upload_path') ?? 'private://webform_strawberryfield/tus/';
+    $this->currentUser = $current_user;
+    $this->eventDispatcher = $event_dispatcher;
+    $this->tusCacheService = $WebformStrawberryTusCacheService;
+    if (!$this->fileSystem->prepareDirectory($this->tusCacheDir, FileSystemInterface::CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS)) {
+      throw new HttpException(500, sprintf('TUS Upload Path folder "%s" is not writable.', $this->tusCacheDir));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getServer(string $upload_key = '', array $post_data = [], string $route_path): Server {
+    $server = $this->getTusServer($upload_key, $route_path);
+
+    // These methods won't pass metadata about the file, and don't need
+    // file directory, because they are reading from TUS cache, so we
+    // can return the server now.
+    $request_method = strtoupper($server->getRequest()->method());
+    if (in_array($request_method, ['GET', 'HEAD', 'PATCH'], TRUE)) {
+      return $server;
+    }
+
+    // Get upload key for directory creation. On POST, it isn't passed, but
+    // we need to add the UUID to file directory to ensure we don't
+    // concatenate same-file uploads if client key is lost.
+    if ($request_method === 'POST') {
+      $upload_key = $server->getUploadKey();
+    }
+
+    // Get the file destination. WE NEE TO MOVE THIS TO THE CONTROLLER.
+    // THAT IS WHERE WE HAVE INFO ABOUT THE WEBFORM/KEY.
+    $destination = $this->determineDestination($upload_key, $post_data);
+    // Set the upload directory for TUS.
+    $server->setUploadDir($destination);
+
+    return $server;
+  }
+
+  /**
+   * Get a configured instance of Tus Server.
+   *
+   * @param string $upload_key
+   *   Upload key from tus if available.
+   * @param string $path
+   *   The actual path we are serving from. Reason is our paths are variable
+   *   Based on the Webform and the upload Key.
+   *
+   * @return \TusPhp\Tus\Server
+   *   Configured instance of Tus server
+   *
+   * @throws \ReflectionException
+   */
+  protected function getTusServer(string $upload_key = '', string $route_path): Server {
+
+    // Initialize TUS server using our shared temp store cache wrapper.
+    $server = new Server($this->tusCacheService);
+    $server->setApiPath($route_path);
+
+    // Get the file destination. WE NEE TO MOVE THIS TO THE CONTROLLER.
+    // THAT IS WHERE WE HAVE INFO ABOUT THE WEBFORM/KEY.
+    $server->setMaxUploadSize((int) 100 * 1048576);
+
+    if ($upload_key) {
+      $server->setUploadKey($upload_key);
+    }
+
+    $server->event()->addListener(UploadComplete::NAME, [
+      $this,
+      'uploadComplete'
+    ]);
+
+    return $server;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function uploadComplete(TusEvent $event): void {
+    $tus_file = $event->getFile();
+    $file_name = $tus_file->getName();
+    $file_path = $tus_file->getFilePath();
+    $metadata = $tus_file->details()['metadata'];
+
+    // MIGHT WANT TO MOVE THIS TO THE CONTROLLER. OR PASS IN THE EVENT THE WEBFORM
+    // WHICH MEANS THE CALLER TO getServer might need to call it with the element info resolved.
+    // OR THE WEBFORM ENTITY ITSELF AND THE KEY.
+    // CHECK THE WEBFORM UPLOAD CODE THAT DOES THOSE CALCULATIONS.
+    $allowed_extensions = $field_definition->getSettings()['file_extensions'];
+    $regex = '/\.(' . preg_replace('/ +/', '|', preg_quote($allowed_extensions)) . ')$/i';
+    if (!preg_match($regex, $metadata['filename'])) {
+      throw new UnprocessableEntityHttpException(sprintf('Only files with the following extensions are allowed: %s.', $allowed_extensions));
+    }
+
+    // Check if the file already exists.
+    $file_query = $this->entityTypeManager->getStorage('file')->getQuery();
+    $file_query->condition('uri', $file_path);
+    $file_query->accessCheck(TRUE);
+    $results = $file_query->execute();
+
+    if (!empty($results)) {
+      // File already exists, just add usage.
+      $file = reset($results);
+      $this->fileUsage->add($file, 'tus', 'file', $file->id());
+      return;
+    }
+
+    /** @var \Drupal\file\FileInterface $file */
+    $file = File::create([
+      'uid' => $this->currentUser->id(),
+      'filename' => $file_name,
+      'uri' => $file_path,
+      'filemime' => mime_content_type($file_path),
+      'filesize' => filesize($file_path),
+    ]);
+    $file->save();
+    // NOT SURE ABOUT THIS...
+    $this->fileUsage->add($file, 'tus', 'file', $file->id());
+
+    // Dispatch an event for other modules to act on.
+    $event = new WebformStrawberryFieldTusUploadedEvent($file);
+    $this->eventDispatcher->dispatch($event);
+  }
+
+}
